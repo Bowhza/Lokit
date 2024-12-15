b7c9181311ed74c9bd9b8e9692312d5329e07c64
Bowhza <bogtank2003@gmail.com>
2024-12-14 17:39:36 -0700

    Made navigation sticky, updated more styles.

--- static/js/script.js
+++ static/js/script.js
@@ -91,6 +91,15 @@ $(()=>{
             }
         }
     })
+
+    const header = document.querySelector("header");
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 30) {
+                header.classList.add("scrolled");
+            } else {
+                header.classList.remove("scrolled");
+            }
+        });
 })
 
 function changePassValidation(){
